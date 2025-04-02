1a55e78063065dbc2bfb230f397b9b8dbc38c005
tollkamlesh <tollkamlesh@gmail.com>
2025-04-02 02:43:39 +0000

    Add 23.rs

---
+++ src/23.rs
@@ -0,0 +1,13 @@
+// This file contains some basic Rust code that you can start working on.
+// Feel free to use it as a starting point or modify it to suit your needs.
+// Some examples of how this code might be used include:
+
+// Example: printing "Hello, World!"
+fn main() {
+    println!("Hello, World!");
+}
+
+// Another example: adding two numbers
+fn add_two_numbers(a: i32, b: i32) -> i32 {
+    a + b
+}
