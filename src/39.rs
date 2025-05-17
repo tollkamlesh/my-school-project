64d53404787af55a86dd9bc4dad6cf65ee90b4ed
tollkamlesh <tollkamlesh@gmail.com>
2025-05-17 11:27:06 +0000

    Add 39.rs

---
+++ src/39.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 5;
+    if x > 0 {
+        println!("x is positive.");
+    } else if x < 0 {
+        println!("x is negative.");
+    } else {
+        println!("x is zero.");
+    }
+}
